645a95b50443e29a070c1c634869b7f6922ddbfe
robinyjpark <robinyjpark@gmail.com>
2022-05-04 17:35:21 +0100

    Add decile plots and recency analyses

--- analysis/.ipynb_checkpoints/validation_histograms-checkpoint.py
+++ analysis/.ipynb_checkpoints/validation_histograms-checkpoint.py
@@ -10,6 +10,8 @@ from ebmdatalab import charts
 from functools import reduce
 from matplotlib import pyplot as plt
 
+from lib_phenotype_validation import *
+
 ############################ CONFIGURE OPTIONS HERE ################################
 
 # Import file
@@ -55,98 +57,39 @@ output_path = 'histograms'
 
 #####################################################################################
 
-def import_clean(input_path, definitions, other_vars, demographic_covariates, 
-                 clinical_covariates, null, date_min, date_max, 
-                 time_delta, output_path, code_dict='', dates=False):
-    # Import
-    df_import = pd.read_feather(input_path)
-    # Dates
-    if dates==True:
-        date_vars = [definition+'_date' for definition in definitions]
-        # Create variable that captures difference in measurement dates
-        date_diff_vars = []
-        # Define start and end dates
-        start_date = datetime.datetime.strptime(date_min, '%Y-%m-%d')
-        end_date = datetime.datetime.strptime(date_max, '%Y-%m-%d')
-        for definition in definitions:
-            # Remove OpenSAFELY null dates 
-            df_import.loc[df_import[definition+'_date'] == '1900-01-01', definition+'_date'] = np.nan
-            # Limit to period of interest             
-            df_import[definition+'_date'] = pd.to_datetime(df_import[definition+'_date'])
-            df_import.loc[df_import[definition+'_date'] < start_date, definition+'_date'] = np.nan
-            df_import.loc[df_import[definition+'_date'] > end_date, definition+'_date'] = np.nan
-            # Remove the measurement if outside the date parameters
-            df_import.loc[df_import[definition+'_date'].isna(), definition] = np.nan
-            df_import 
-            # Create difference between measurement dates
-            df_import[definition+'_date'] = df_import[definition+'_date'].dt.to_period(time_delta).dt.to_timestamp()
-            df_import = df_import.sort_values(by=['patient_id',definition+'_date'])
-            df_import['date_diff_' + definition] = round(df_import.groupby('patient_id')[definition+'_date'].diff() / np.timedelta64(1, time_delta))
-            date_diff_vars.append('date_diff_' + definition)
-    else: 
-        date_vars = []
-        date_diff_vars = []
-    # Codes
-    if code_dict!='':
-        for key in code_dict:
-            df_import[key] = df_import[key].astype(float)
-            df_import[key] = df_import[key].replace(code_dict[key])
-    
-    # Subset to relevant columns
-    df_clean = df_import[['patient_id'] + definitions + other_vars + date_vars + date_diff_vars + demographic_covariates + clinical_covariates]
-    # Limit to relevant date range
-    df_clean = df_clean.sort_values(by='patient_id').reset_index(drop=True)
-    # Set null values to nan
-    for definition in definitions: 
-        df_clean.loc[df_clean[definition].isin(null), definition] = np.nan
-     # Create order for categorical variables
-    for group in demographic_covariates + clinical_covariates:
-        if df_clean[group].dtype.name == 'category':
-            li_order = sorted(df_clean[group].dropna().unique().tolist())
-            df_clean[group] = df_clean[group].cat.reorder_categories(li_order, ordered=True)
-    # Mark patients with value filled/missing for each definition
-    li_filled = []
-    for definition in definitions:
-        df_fill = pd.DataFrame(df_clean.groupby("patient_id")[definition].any().astype('int')).rename(
-            columns={definition:definition+'_filled'}
-        )
-        df_fill[definition+'_missing'] = 1-df_fill[definition+'_filled']
-        li_filled.append(df_fill)
-
-    df_filled = pd.concat(li_filled, axis=1)
-    # Remove list from memory
-    del li_filled  
-    df_clean = df_clean.merge(df_filled, on='patient_id')
-    
-    # Flag all filled/all missing
-    li_col_filled = [col for col in df_clean.columns if col.endswith('_filled')]
-    li_col_missing = [col for col in df_clean.columns if col.endswith('_missing')]
-    df_clean['all_filled'] = (df_clean[li_col_filled].sum(axis=1) == len(definitions)).astype(int)
-    df_clean['all_missing'] = (df_clean[li_col_missing].sum(axis=1) == len(definitions)).astype(int)
-    
-    # Check whether output paths exist or not, create if missing
-    path_tables = f'output/{output_path}/tables'
-    path_figures = f'output/{output_path}/figures'
-    li_filepaths = [path_tables, path_figures]
-
-    for filepath in li_filepaths:
-        exists = os.path.exists(filepath)
-        if not exists:
-            os.makedirs(filepath)
-
-    return df_clean
+def redact_round_table2(df_in):
+    """Redacts counts <= 5 and rounds counts to nearest 5"""
+    df_out = df_in.where(df_in > 5, np.nan).apply(lambda x: 5 * round(x/5) if ~np.isnan(x) else x)
+    return df_out
 
 def hist(df_in, measure, title, path):
     # 30 bins
     try: 
         df_hist = pd.DataFrame(pd.cut(df_in[measure], 30).value_counts().sort_index()).reset_index().rename(columns={'index':'intervals'})
+        df_hist[measure] = df_hist[measure].where(
+            df_hist[measure]> 5, np.nan).apply(lambda x: 5 * round(x/5) if ~np.isnan(x) else x)
         df_in['bin']=pd.cut(df_in[measure], bins = 30).astype(str)
-        df_in2 = df_in.groupby('bin').bin.count()
-        # Fixed to show distribution of bin
+        df_in2 = redact_round_table2(df_in.groupby('bin').bin.count())
         df_in2.plot(kind='bar')
         plt.title(title)
         df_hist.to_csv(f'output/{output_path}/tables/hist_data_{path}.csv')
         plt.savefig(f'output/{output_path}/figures/hist_{path}.png', bbox_inches="tight")
+        plt.close()
+    except:
+        pass
+
+def decile_plot(df_in, measure, title, path):
+    try:
+        df_dec = pd.DataFrame(pd.qcut(df_in[measure], 10, duplicates='drop').value_counts().sort_index()).reset_index().rename(columns={'index':'intervals'})
+        df_dec[measure] = df_dec[measure].where(
+            df_dec[measure]> 5, np.nan).apply(lambda x: 5 * round(x/5) if ~np.isnan(x) else x)
+        df_in['bin'] = pd.qcut(df_in[measure], 10, duplicates='drop').astype(str)
+        df_in2 = redact_round_table2(df_in.groupby('bin').bin.count())
+        df_in2.plot(kind='bar')
+        plt.title(title)
+        df_dec.to_csv(f'output/{output_path}/tables/decile_plot_{path}.csv')
+        plt.savefig(f'output/{output_path}/figures/decile_plot_{path}.png', bbox_inches="tight")
+        plt.close()
     except:
         pass
     
@@ -159,7 +102,6 @@ def subset_q(df_in, measure, pct, less=True):
     if pct < 1:
         p = q_n(df_clean[measure], pct)
     else:
-        less = False
         p = pct
     if less == True:
         df_p = df_clean.loc[df_clean[measure] < p]
@@ -183,17 +125,9 @@ def main():
     hist(df_p5, 'weight_backend', 'Weight for Patients with 5% Lowest BMI', 'weight_p5')
     hist(df_p5, 'height_backend', 'Height for Patients with 5% Lowest BMI', 'height_p5')
     # Highest 5%
-    df_p95 = subset_q(df_clean, 'backend_bmi', 0.95)
+    df_p95 = subset_q(df_clean, 'backend_bmi', 0.95, less=False)
     hist(df_p95, 'weight_backend', 'Weight for Patients with 5% Highest BMI', 'weight_p95')
     hist(df_p95, 'height_backend', 'Height for Patients with 5% Highest BMI', 'height_p95')
-    # Lowest 10%
-    df_p10 = subset_q(df_clean, 'backend_bmi', 0.1)
-    hist(df_p10, 'weight_backend', 'Weight for Patients with 10% Lowest BMI', 'weight_p10')
-    hist(df_p10, 'height_backend', 'Height for Patients with 10% Lowest BMI', 'height_p10')
-    # Highest 10%
-    df_p90 = subset_q(df_clean, 'backend_bmi', 0.9)
-    hist(df_p90, 'weight_backend', 'Weight for Patients with 10% Highest BMI', 'weight_p90')
-    hist(df_p90, 'height_backend', 'Height for Patients with 10% Highest BMI', 'height_p90')
     # Lower than minimum expected range
     df_min = subset_q(df_clean, 'backend_bmi', 4)
     hist(df_min, 'weight_backend', 'Weight for Patients with BMI < 4', 'weight_lt_min')
@@ -202,7 +136,31 @@ def main():
     df_max = subset_q(df_clean, 'backend_bmi', 200, False)
     hist(df_max, 'weight_backend', 'Weight for Patients with BMI > 200', 'weight_gt_max')
     hist(df_max, 'height_backend', 'Height for Patients with BMI > 200', 'height_gt_max')
-        
+    # Histogram of negative values
+    df_height_neg = df_clean.loc[df_clean['height_backend'] < 0]
+    hist(df_height_neg, 'height_backend', 'Distribution of Negative Heights', 'height_negative')
+    df_weight_neg = df_clean.loc[df_clean['weight_backend'] < 0]
+    hist(df_weight_neg, 'weight_backend', 'Distribution of Negative Weights', 'weight_negative')
+    ### Create decile plots
+    # All population
+    decile_plot(df_clean, 'weight_backend', 'Weight (CTV3 Codes Used in OpenSAFELY-TPP Backend)', 'weight_all')
+    decile_plot(df_clean, 'height_backend', 'Height (CTV3 Codes Used in OpenSAFELY-TPP Backend)', 'height_all')
+    # Lowest 5%
+    decile_plot(df_p5, 'weight_backend', 'Weight for Patients with 5% Lowest BMI', 'weight_p5')
+    decile_plot(df_p5, 'height_backend', 'Height for Patients with 5% Lowest BMI', 'height_p5')
+    # Highest 5%
+    decile_plot(df_p95, 'weight_backend', 'Weight for Patients with 5% Highest BMI', 'weight_p95')
+    decile_plot(df_p95, 'height_backend', 'Height for Patients with 5% Highest BMI', 'height_p95')
+    # Lower than minimum expected range
+    decile_plot(df_min, 'weight_backend', 'Weight for Patients with BMI < 4', 'weight_lt_min')
+    decile_plot(df_min, 'height_backend', 'Height for Patients with BMI < 4', 'height_lt_min')
+    # Higher than maximum expected range
+    decile_plot(df_max, 'weight_backend', 'Weight for Patients with BMI > 200', 'weight_gt_max')
+    decile_plot(df_max, 'height_backend', 'Height for Patients with BMI > 200', 'height_gt_max')
+    # Negative values
+    decile_plot(df_height_neg, 'height_backend', 'Distribution of Negative Heights', 'height_negative')
+    decile_plot(df_weight_neg, 'weight_backend', 'Distribution of Negative Weights', 'weight_negative')
+    
 ########################## DO NOT EDIT – RUNS SCRIPT ##############################
 
 if __name__ == "__main__":
