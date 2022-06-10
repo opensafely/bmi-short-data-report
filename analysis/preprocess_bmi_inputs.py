008cf9fee1f279f543f67d3f9ebdf3f849ceec98
robinyjpark <robinyjpark@gmail.com>
2022-06-10 14:57:58 +0100

    fix: differentiate renaming of BMI date columns

--- analysis/preprocess_bmi_inputs.py
+++ analysis/preprocess_bmi_inputs.py
@@ -7,9 +7,14 @@ df_input = pd.read_feather(f'output/data/input_{bmi}.feather')
 
 # Change variable names 
 for n in range(1,11):
-    df_input = df_input.rename(
-        columns={f'{bmi}{n}_date_measured':f'{bmi}_date{n}',}
-    )
+    if bmi == 'derived_bmi':
+        df_input = df_input.rename(
+            columns={f'{bmi}{n}_date_measured':f'{bmi}_date{n}',}
+        )
+    elif bmi == 'recorded_bmi':
+        df_input = df_input.rename(
+            columns={f'{bmi}{n}_date':f'{bmi}_date{n}',}
+        )
 
 # Reshape wide to long
 df_output = pd.wide_to_long(
