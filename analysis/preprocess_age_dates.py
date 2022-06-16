89c80d9ac6385116c506f8165ea691d9723a27e4
robinyjpark <robinyjpark@gmail.com>
2022-06-16 11:21:15 +0100

    fix: adjust covariate flags based on measurement dates

--- analysis/preprocess_age_dates.py
+++ analysis/preprocess_age_dates.py
@@ -3,8 +3,12 @@ import pandas as pd
 for bmi in ['backend_computed_bmi','computed_bmi','derived_bmi','recorded_bmi']:
     df_bmi = pd.read_feather(f'output/joined/input_processed_{bmi}.feather')
 
-    # Drop measurements with invalid dates 
-    df_bmi = df_bmi.loc[df_bmi[f'{bmi}_date'] > '1900-01-01'].reset_index(drop=True)
+    # Only keep measurements with valid dates 
+    df_bmi = df_bmi.loc[
+        (df_bmi[f'{bmi}_date'] > '1900-01-01') | # after default date 
+        (df_bmi[f'{bmi}_date'] < df_bmi['dereg_date']) | # before deregistration
+        (df_bmi[f'{bmi}_date'] < df_bmi['died_date_ons']) # before death
+    ].reset_index(drop=True)
 
     # Create age
     df_bmi['date_of_birth'] = pd.to_datetime(df_bmi['date_of_birth'])
@@ -24,5 +28,9 @@ for bmi in ['backend_computed_bmi','computed_bmi','derived_bmi','recorded_bmi']:
     df_bmi.loc[(df_bmi['age'] >=60) & (df_bmi['age'] < 70), 'age_band'] = '60-69'
     df_bmi.loc[(df_bmi['age'] >=70) & (df_bmi['age'] < 80), 'age_band'] = '70-79'
     df_bmi.loc[(df_bmi['age'] >=80) & (df_bmi['age'] < 120), 'age_band'] = '80+'
+
+    # Adjust clinical covariates based on date
+    for cov in ['dementia', 'diabetes', 'learning_disability']:
+        df_bmi.loc[df_bmi[f'{bmi}_date'] < df_bmi[f'{cov}_date'], f'{cov}'] = False
 
     df_bmi.to_feather(f'output/joined/input_processed_{bmi}.feather')
