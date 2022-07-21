75c6a2b1daab63daa5ddc35bbd1f5fdebf2cf583
robinyjpark <robinyjpark@gmail.com>
2022-07-21 13:55:05 +0100

    fix: reconfigure age brackets

--- analysis/preprocess_age_dates.py
+++ analysis/preprocess_age_dates.py
@@ -18,10 +18,12 @@ for bmi in ['backend_computed_bmi','computed_bmi','derived_bmi','recorded_bmi']:
         (df_bmi['date_of_birth'].dt.month)
     )
 
+    # Keep if age >= 18
+    df_bmi = df_bmi.loc[df_bmi['age'] >= 18]
+
     # Create age_band
     df_bmi['age_band'] = 'missing'
-    df_bmi.loc[(df_bmi['age'] >=0) & (df_bmi['age'] < 20), 'age_band'] = '0-19'
-    df_bmi.loc[(df_bmi['age'] >=20) & (df_bmi['age'] < 30), 'age_band'] = '20-29'
+    df_bmi.loc[(df_bmi['age'] >=18) & (df_bmi['age'] < 30), 'age_band'] = '18-29'
     df_bmi.loc[(df_bmi['age'] >=30) & (df_bmi['age'] < 40), 'age_band'] = '30-39'
     df_bmi.loc[(df_bmi['age'] >=40) & (df_bmi['age'] < 50), 'age_band'] = '40-49'
     df_bmi.loc[(df_bmi['age'] >=50) & (df_bmi['age'] < 60), 'age_band'] = '50-59'
